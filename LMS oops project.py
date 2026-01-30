51d100390f0f8a01a1e4141c7cb4cf40554b1ba3
Bhargavi <mbhargavi1113@gmail.com>
2026-01-30 09:25:13 +0530

    Add files via upload

--- LMS oops project.py
+++ LMS oops project.py
@@ -1,22 +1,39 @@
 #parent class
 class LibraryItem:
+    
+    #Constructor
+    #Encapsulation
     def __init__(self,item_name,item_title):
         self.item_name = item_name
         self.item_title = item_title
+        
+    #Abstraction
     def borrowing_charges(self,borrow_days):
         pass
+    
 #child class of book
+#Inheritance
 class Book(LibraryItem):
+    
+    #polymorphism
     def borrowing_charges(self, borrow_days):
         return borrow_days*10
+    
 #child class of magazine
+#inheritance
 class Magazine(LibraryItem):
+    
+    #Polymorphism
     def borrowing_charges(self, borrow_days):
         return borrow_days*10
     
 class LibraryApp:
+    
+    #has-a relationship
     def __init__(self):
         self.item = None
+        
+    #abstraction
     def create_item(self,item_name,item_title,item_type):
         if item_type == "Book":
             self.item = Book(item_name,item_title)
@@ -26,10 +43,13 @@ class LibraryApp:
             return "Item type: Magazine"
         else:
             self.item = None
+    
+    #polymorphism
     def borrowing_charges(self,borrow_days):
         charges = self.item.borrowing_charges(borrow_days)
         return charges
     
+#object creation
 app = LibraryApp()
 
 print(app.create_item(1,"Programming in Python","Book"))
